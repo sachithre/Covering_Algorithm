1c17602e43d050eb91a2e2f5f87eb5be7844deda
wsachithregit <wsachithre@gmail.com>
2019-07-03 00:50:09 +0530

    First Commit

---
+++ CoveringAlgorithm/src/CoveringAlgorithm.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import weka.associations.Apriori;
+
+import weka.core.Instances;
+
+public class CoveringAlgorithm {
+
+	public static void main(String[] args) throws Exception {
+		FileReader reader = new FileReader("C:/Users/cst15053/Desktop/weather.arff"); // read the dataset file
+		Instances instance_train = new Instances(reader); //create an instance
+		BufferedReader fi = new BufferedReader(reader); //read the file and pass data to buffer
+		fi.close();
+		instance_train.setClassIndex(4);
+		
+		Apriori c = new Apriori();
+		String []options= {"-N 15", "-C 1.0"};
+		c.setOptions(options);
+		c.buildAssociations(instance_train);
+		
+		System.out.print(c.toString());
+		
+
+	}
+
+}
---
+++ CoveringAlgorithm/src/LinearReg.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import weka.classifiers.functions.LinearRegression;
+import weka.core.Instances;
+
+public class LinearReg {
+
+	public static void main(String[] args) throws Exception {
+
+		FileReader reader = new FileReader("C:/Users/cst15053/Desktop/weatherN.arff"); // read the dataset file
+		Instances instance_train = new Instances(reader); //create an instance
+		BufferedReader fi = new BufferedReader(reader); //read the file and pass data to buffer
+		fi.close();
+		instance_train.setClassIndex(1);
+		
+		LinearRegression lr = new LinearRegression();
+		lr.buildClassifier(instance_train);
+		
+		System.out.print(lr);
+		
+	}
+
+}
